6c667bd877ed6b5cc51a7949042898ca6b37efeb
ivan.diazbonansea@dynatrace.com <ivan.diazbonansea@dynatrace.com>
2023-07-28 19:28:49 +0200

    first commit

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <span>hello header</span>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
