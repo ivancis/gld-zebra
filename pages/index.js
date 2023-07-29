858600fb10136804740e35f8168f787ea8707cd2
ivan.diazbonansea@dynatrace.com <ivan.diazbonansea@dynatrace.com>
2023-07-29 10:35:21 +0200

    styling

--- pages/index.js
+++ pages/index.js
@@ -33,73 +33,110 @@ export default function Index({ posts, globalData }) {
             <h2 class="text-5xl font-bold">Hi, I&apos;m Gonzalo.</h2>
             <p class="text-2xl">I&apos;m a professional English into Spanish translator specializing in Game Localization.</p>
           </div>
+        </div>
+
+
+
+          <div class="slideshow-columns">
+            <div class="columns-A">
+              <span>gameThumbnail</span>
+            </div>
+            <div class="columns-B">
+              <span>gameThumbnail</span>
+              <span>gameThumbnail</span>
+            </div>
+            <div class="columns-C">
+              <span>gameThumbnail</span>
+              <span>gameThumbnail</span>
+            </div>
+            <div class="columns-D">
+              <span>gameThumbnail</span>
+            </div>
+          </div>
+
+
+
+        <div class="flex flex-col gap-4 max-w-4xl w-full mx-auto p-8 mb-8">
           <blockquote class="text-l italic border-l-2 ml-1 pb-2 pl-5 pr-0 text-gray-900 dark:text-white">
             <p>&apos;I have years of experience in projects for various platforms, and I&apos;ve been a gamer since I was able to pick up a controller. My professional journey has led me to appreciate video games as the most complex art form of our time, and as such, they require one of the most comprehensive translation processes.&apos;</p>
           </blockquote>
         </div>
 
-        <div class="max-w-7xl w-full mx-auto flex flex-wrap gap-4 p-2">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <div class="max-w-7xl w-full mx-auto flex flex-wrap justify-center gap-4 p-2">
+          <div class="max-w-sm w-full p-6 rounded-lg backdrop-blur-xl bg-white dark:bg-black dark:bg-opacity-40 bg-opacity-30">
+            <svg class="w-5 h-5 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
             </svg>
             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
               Translation Services
             </h5>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            <p class="mb-3 text-sm">
               Translation, Localization, Transcreation, Editing, Proofreading and LQA. I can offer my expertise to effectively help your games reach the Spanish-speaking community and expand into new markets.
             </p>
           </div>
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <div class="max-w-sm w-full p-6 rounded-lg backdrop-blur-xl bg-white dark:bg-black dark:bg-opacity-40 bg-opacity-30">
+            <svg class="w-5 h-5 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
             </svg>
             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
               PC, Consoles & Mobile
             </h5>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            <p class="mb-3 text-sm">
               I&apos;ve translated over 1 million words across all available platforms, which has allowed me to master their best practices and learn their specific terminology.
             </p>
           </div>
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <div class="max-w-sm w-full p-6 rounded-lg backdrop-blur-xl bg-white dark:bg-black dark:bg-opacity-40 bg-opacity-30">
+            <svg class="w-5 h-5 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
             </svg>
             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
               Gamer Vision
             </h5>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            <p class="mb-3 text-sm">
               Gamers don&apos;t tolerate mediocrity and know how to appreciate quality. A well-designed game must understand its audience and meet cultural and linguistic expectations. I can provide the much-needed gamer input for a successful localization.
             </p>
           </div>
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <div class="max-w-sm w-full p-6 rounded-lg backdrop-blur-xl bg-white dark:bg-black dark:bg-opacity-40 bg-opacity-30">
+            <svg class="w-5 h-5 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
             </svg>
             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
               Glossary Creation
             </h5>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            <p class="mb-3 text-sm">
               I can create a glossary or termbase from scratch to make the localization of your game increasingly streamlined and error-free.
             </p>
           </div>
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <div class="max-w-sm w-full p-6 rounded-lg backdrop-blur-xl bg-white dark:bg-black dark:bg-opacity-40 bg-opacity-30">
+            <svg class="w-5 h-5 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
             </svg>
             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
               CATs
             </h5>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            <p class="mb-3 text-sm">
               MemoQ, Memsource (Phrase), SmartCat
             </p>
           </div>
         </div>
 
-        <div>
-          <h2 class="text-5xl text-center">+1,000,000 WORDS TRANSLATED</h2>
-        </div>
 
+        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div class="flex flex-col md:flex-row flex-wrap gap-8 justify-center">
+            <div class="text-center">
+              <h6 class="text-3xl font-bold text-deep-purple-accent-400">+1,000,000</h6>
+              <p class="">Words translated</p>
+            </div>
+            <div class="text-center">
+              <h6 class="text-3xl font-bold text-deep-purple-accent-400">NN</h6>
+              <p class="">Localisation projects</p>
+            </div>
+            <div class="text-center">
+              <h6 class="text-3xl font-bold text-deep-purple-accent-400">NN years</h6>
+              <p class="">In the localisation industry</p>
+            </div>
+          </div>
+        </div>
 
 
 
